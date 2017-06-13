fa916da0651f69d7478a608a7dad430135b68b75
wuminlai <wml@kandaovr.com>
2017-06-13 20:06:36 +0800

    support for download other types file

--- m3u8.py
+++ m3u8.py
@@ -5,17 +5,15 @@ from __future__ import unicode_literals
 from gevent import monkey
 monkey.patch_all()
 from gevent.pool import Pool
-import gevent
 import requests
 import urllib.parse
 import urllib.request
-import os
+import os, json
 import time
 
 
 class Downloader:
     def __init__(self, pool_size, retry=3):
-        self.pool = Pool(pool_size)
         self.session = self._get_http_session(pool_size, pool_size, retry)
         self.retry = retry
         self.dir = ''
@@ -30,7 +28,7 @@ class Downloader:
             session.mount('https://', adapter)
             return session
 
-    def run(self, m3u8_url, dir=''):
+    def run(self, m3u8_url, dir='tmp'):
         self.dir = dir
         if self.dir and not os.path.isdir(self.dir):
             os.makedirs(self.dir)
@@ -40,14 +38,7 @@ class Downloader:
             body = r.content.decode("utf-8")
             if body:
                 ts_list = [urllib.parse.urljoin(m3u8_url, n.strip()) for n in body.split('\n') if n and not n.startswith("#")]
-                print(ts_list)
-                # ts_list = zip(ts_list, [n for n in range(len(ts_list))])
                 if ts_list:
-                    # self.ts_total = len(ts_list)
-                    # print(self.ts_total)
-                    # g1 = gevent.spawn(self._join_file)
-                    # self._download(ts_list)
-                    # g1.join()
                     for k in ts_list:
                         self.download(k, self.dir)
         else:
@@ -55,7 +46,6 @@ class Downloader:
 
     def download(self, url, path):
         base_url, origin_name = os.path.split(url)
-        print(base_url, origin_name)
         save_path = os.path.join(path, origin_name)
         response = urllib.request.urlretrieve(url=url)
         contents = open(response[0], "br").read()
@@ -65,50 +55,66 @@ class Downloader:
         f.write(contents)
         f.close()
 
-    def _download(self, ts_list):
-        self.pool.map(self._worker, ts_list)
-        if self.failed:
-            ts_list = self.failed
-            self.failed = []
-            self._download(ts_list)
 
-    def _worker(self, ts_tuple):
-        url = ts_tuple[0]
-        index = ts_tuple[1]
-        retry = self.retry
-        while retry:
-            try:
-                r = self.session.get(url, timeout=20)
-                if r.ok:
-                    file_name = url.split('/')[-1]
-                    print(file_name)
-                    with open(os.path.join(self.dir, file_name), 'wb') as f:
-                        f.write(r.content)
-                    self.succed[index] = file_name
-                    return
-            except:
-                retry -= 1
-        # print('[FAIL]%s' % url)
-        self.failed.append((url, index))
+class Parser:
+    def __init__(self, dir='tmp'):
+        self.dir = dir
+        if self.dir and not os.path.isdir(self.dir):
+            os.makedirs(self.dir)
 
-    def _join_file(self):
-        index = 0
-        outfile = ''
-        while index < self.ts_total:
-            file_name = self.succed.get(index, '')
-            if file_name:
-                infile = open(os.path.join(self.dir, file_name), 'rb')
-                if not outfile:
-                    outfile = open(os.path.join(self.dir, file_name.split('.')[0]+'_all.'+file_name.split('.')[-1]), 'wb')
-                outfile.write(infile.read())
-                infile.close()
-                os.remove(os.path.join(self.dir, file_name))
-                index += 1
+    def prase(self, http_url):
+        response = requests.get(url=http_url, verify=False)
+        if response.status_code == 200:
+            jsondata = json.loads(response.content.decode('utf-8'))
+
+            playlist = jsondata["playlist"]
+            for k in playlist["videos"]:
+                self.download(k["uri"])
+                self.download(k["thumbnail"])
+                self.download(k["download_uri"])
+        else:
+            print("http error %d" % response.status_code)
+
+    def download(self, http_link):
+        ext = os.path.splitext(http_link)[1]
+        if ext == ".m3u8":
+            session = requests.Session()
+            adapter = requests.adapters.HTTPAdapter(pool_connections=50, pool_maxsize=50,max_retries=3)
+            session.mount('http://', adapter)
+            session.mount('https://', adapter)
+            r = session.get(http_link, timeout=10)
+            if r.ok:
+                body = r.content.decode("utf-8")
+                if body:
+                    ts_list = [urllib.parse.urljoin(http_link, n.strip()) for n in body.split('\n') if
+                               n and not n.startswith("#")]
+
+                    if ts_list:
+                        for k in ts_list:
+                            self._download(k, self.dir)
             else:
-                time.sleep(1)
-        if outfile:
-            outfile.close()
+                print(r.status_code)
+
+        else:
+            self._download(http_link, self.dir)
+
+    def _download(self, http_link, dst):
+        print(http_link, dst)
+        base_url, origin_name = os.path.split(http_link)
+        print(base_url, origin_name)
+        save_path = os.path.join(dst, origin_name)
+        print(save_path)
+        response = urllib.request.urlretrieve(url=http_link)
+        contents = open(response[0], "br").read()
+
+        # save path
+        f = open(save_path, "wb")
+        f.write(contents)
+        f.close()
+
 
 if __name__ == '__main__':
-    downloader = Downloader(50)
-    downloader.run('http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear1/prog_index.m3u8', '/home/wuminlai/Work/media/offset_hls/bipbop_4x3/gear1')
+    parse = Parser()
+    parse.prase("https://api.kandaovr.com/video/v1/playlist?codec=4&container=H&language=ZH-CN&name=KANDAO_APP_MAIN&page_count=4&page_num=0&vbr=8&warping=C")
+    #downloader = Downloader(50)
+    #downloader.run('http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_16x9/gear5/prog_index.m3u8', '/home/wuminlai/Work/media/offset_hls/bipbop_16x9/gear5')
